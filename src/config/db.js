86af38cc4b94cb74c760ec43e55de91556150698
joseprm14 <joseprm14@gmail.com>
2025-07-11 18:15:10 +0200

    'actions'

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Definimos la base de datos para sequelize utilizando variables de entorno
-const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
