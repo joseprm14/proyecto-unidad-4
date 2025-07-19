24e32f315824edc2a43073ce38360369215b759a
joseprm14 <joseprm14@gmail.com>
2025-07-19 22:40:43 +0200

    railway

--- src/config/db.js
+++ src/config/db.js
@@ -6,8 +6,8 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === 'production';
 
 // Definimos la base de datos para sequelize utilizando variables de entorno
-if (isProduction) {
-    const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = isProduction ? 
+    new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
         protocol: 'postgres',
         dialectOptions: {
@@ -17,15 +17,12 @@ if (isProduction) {
             },
         },
         logging: false,
-    });
-
-} else {
-    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    }) :
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: 'postgres',
         logging: false,
     });
 
-}
 
 module.exports = sequelize;
